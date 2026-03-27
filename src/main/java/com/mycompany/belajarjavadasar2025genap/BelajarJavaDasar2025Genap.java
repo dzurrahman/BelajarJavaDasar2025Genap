4e4acf3e3d5808b819478d0ebf9cd28e6d016c8c
dzurrahman roki <dzurrahman@gmail.com>
2026-03-27 13:35:46 +0700

    Latihan membuat commit dan push

---
+++ src/main/java/com/mycompany/belajarjavadasar2025genap/BelajarJavaDasar2025Genap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajarjavadasar2025genap;
+
+/**
+ *
+ * @author dzuro
+ */
+public class BelajarJavaDasar2025Genap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
