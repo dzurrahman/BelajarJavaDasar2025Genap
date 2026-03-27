557d92e892eb8db780b39adfdd1698a12c2c8835
dzurrahman roki <dzurrahman@gmail.com>
2026-03-27 13:40:32 +0700

    Membuat Class Loop For

---
+++ src/main/java/com/mycompany/belajarjavadasar2025genap/LoopFor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajarjavadasar2025genap;
+
+/**
+ *
+ * @author dzuro
+ */
+public class LoopFor {
+    public static void main(String[] args) {
+    // i mulai dari 1, selama i <= 5, i bertambah 1
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+        
+        System.out.println("Perulangan selesai.");
+    }
+}
