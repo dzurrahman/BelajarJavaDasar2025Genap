858d2a1b155888fd47ed5a6397398e41268afeee
dzurrahman roki <dzurrahman@gmail.com>
2026-04-17 13:40:28 +0700

    Pertemuan 2: Create Class VariableDemo

---
+++ src/main/java/com/mycompany/belajarjavadasar2025genap/pertemuan2/VariabelDemo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajarjavadasar2025genap.pertemuan2;
+
+/**
+ *
+ * @author dzuro
+ */
+public class VariabelDemo {
+    
+    public static void main(String[] args) {
+        // 1. Deklarasi (Membuat kotak kosong)
+        int umur;
+        
+        // 2. Inisialisasi (Mengisi kotak)
+        umur = 20;
+        
+        // Atau bisa langsung sekali jalan:
+        String nama = "Andi";
+        double tinggi = 175.5;
+        boolean mahasiswaAktif = true;
+        
+        // Menampilkan isi variabel ke layar
+        System.out.println("Data Mahasiswa:");
+        System.out.println(nama); // Hanya menampilkan isi variabel
+        System.out.println(umur);
+        System.out.println(tinggi);
+    }
+}
