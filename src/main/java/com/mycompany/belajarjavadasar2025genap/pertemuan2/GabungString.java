ab655736066ad6c59b342b2cdb9246ee665e47ae
dzurrahman roki <dzurrahman@gmail.com>
2026-04-17 13:42:12 +0700

    Pertemuan 2: Create Class GabungString

---
+++ src/main/java/com/mycompany/belajarjavadasar2025genap/pertemuan2/GabungString.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajarjavadasar2025genap.pertemuan2;
+
+/**
+ *
+ * @author dzuro
+ */
+public class GabungString {
+    
+}
